9e53967d04c38e10593ca3e1017cfc1f248328eb
Weh Andreas <andreas.weh@student.uni-augsburg.de>
2017-12-29 18:26:46 +0100

    Add module description

---
+++ __init__.py
@@ -0,0 +1,6 @@
+"""Collection of commonly used Green's functions and utilities
+
+So far mainly contains Bethe Green's functions.
+Main purpose is to have a tested base.
+
+FIXME: Add version via git!!!"""
