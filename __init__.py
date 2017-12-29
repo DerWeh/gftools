910668fe83395a4445898db6be4a6fafcc990a55
Weh Andreas <andreas.weh@student.uni-augsburg.de>
2017-12-29 19:57:38 +0100

    Fix docstring, add *absolute_import* import

--- __init__.py
+++ __init__.py
@@ -4,17 +4,19 @@ So far mainly contains Bethe Green's functions.
 Main purpose is to have a tested base.
 
 FIXME: Add version via git!!!"""
-from __future__ import print_function, division
+from __future__ import absolute_import, division, print_function
+
 import numpy as np
 
 
 def bethe_dos(eps, half_bandwidth):
-    """DOS of non-interacting Bethe lattice for infinite Coordination number.
+    """DOS of non-interacting Bethe lattice for infinite coordination number.
     
     Works by evaluating the complex function and truncating imaginary part.
+
     Params
     ------
-    eps: array(double)
+    eps: array(double), double
         DOS is evaluated at points *eps*
     half_bandwidth: double
         half bandwidth of the DOS, DOS(|eps| > half_bandwidth) = 0
