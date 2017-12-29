3017d0a7f7a517ec3f165bf3203599281e3c3ed5
Weh Andreas <andreas.weh@student.uni-augsburg.de>
2017-12-29 19:58:05 +0100

    Add *bethe_gf_omega* function

--- __init__.py
+++ __init__.py
@@ -25,3 +25,18 @@ def bethe_dos(eps, half_bandwidth):
     eps = np.array(eps, dtype=np.complex256)
     res = np.sqrt(D**2 - eps**2) / (0.5 * np.pi * D**2)
     return res.real
+
+
+def bethe_gf_omega(z, half_bandwidth):
+    """Local Green's function of Bethe lattice for infinite Coordination number
+    
+    Params
+    ------
+    z: array(complex), complex
+        Green's function is evaluated at complex frequency *z*
+    half_bandwidth: double
+        half-bandwidth of the DOS of the Bethe lattice
+    """
+    D = half_bandwidth
+    return 2.*z*(1 - np.sqrt(1 - (D/z)**2))/D
+    # return 2./z/(1 + np.sqrt(1 - (D**2)*(z**(-2))))
