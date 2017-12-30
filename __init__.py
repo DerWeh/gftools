5df12e1efd18a46b2d0087c14253444c475703d7
Weh Andreas <andreas.weh@student.uni-augsburg.de>
2017-12-30 12:36:21 +0100

    Add function *bethe_hilbert_transfrom*

--- __init__.py
+++ __init__.py
@@ -41,3 +41,43 @@ def bethe_gf_omega(z, half_bandwidth):
     D = half_bandwidth
     return 2.*z*(1 - np.sqrt(1 - (D/z)**2))/D
     # return 2./z/(1 + np.sqrt(1 - (D**2)*(z**(-2))))
+
+
+def bethe_hilbert_transfrom(xi, half_bandwidth):
+    r"""
+    Hilbert transform of non-interacting DOS of the Bethe lattice.
+
+    The Hilbert transform
+    :math:`\tilde{D}(\xi) = \int_{-{}\infty}^{\infty}d\epsilon \frac{DOS(\epsilon)}{\xi - \epsilon}`
+    takes for Bethe lattice in the limit of infinite coordination number the
+    explicit form
+
+    .. math::
+
+        \tilde{D}(\xi) = 2*(\xi - s\sqrt{\xi^2 - D^2})/D^2
+
+    with :math:`s=sgn[\Im{\xi}]`.
+
+    see Georges_
+
+
+    Params
+    ------
+    xi: array(complex), complex
+        Point at which the Hilbert transform is evaluated
+    half_bandwidth: double
+        half-bandwidth of the DOS of the Bethe lattice
+
+    .. _Georges: https://journals.aps.org/rmp/abstract/10.1103/RevModPhys.68.13
+
+    Notes
+    -----
+    Relation between nearest neighbor hopping t and half-bandwidth D:
+
+    .. math::
+        2t = D
+    """
+    xi_rel = xi/half_bandwidth
+    sgn = np.sign(xi_rel.imag)
+    # sgn = np.sign(np.imag(-1j*np.sqrt(1- xi**2)))  # Junya's version
+    return 2*(xi_rel - 1j*sgn*np.sqrt(1 - xi_rel**2))/half_bandwidth
