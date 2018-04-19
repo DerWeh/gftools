48870080549999baeff4fac97076790c2620debd
Weh Andreas <andreas.weh@student.uni-augsburg.de>
2018-04-19 16:11:45 +0200

    Add basic unit-test for `density`

---
+++ gftools/tests/test_utilities.py
@@ -0,0 +1,36 @@
+# coding: utf8
+"""Test the utility functions related to Green's functions."""
+from __future__ import absolute_import, unicode_literals
+
+import pytest
+
+import numpy as np
+
+from .context import gftools
+
+
+def test_density():
+    """Check density for simple Green's functions."""
+    beta = 10.1
+    D = 1.2
+    iw_array = gftools.matsubara_frequencies(np.arange(int(2**14)), beta=beta)
+
+    #
+    # Bethe lattice Gf
+    #
+    bethe_half = gftools.bethe_gf_omega(iw_array, half_bandwidth=D)
+    assert gftools.density(bethe_half, potential=0, beta=beta) == 0.5
+
+    large_shift = 10000*D
+    # Bethe lattice almost filled (depends on small temperature)
+    bethe_full = gftools.bethe_gf_omega(iw_array+large_shift, half_bandwidth=D)
+    assert gftools.density(bethe_full, potential=large_shift, beta=beta) == pytest.approx(1.0)
+    # Bethe lattice almost empty (depends on small temperature)
+    bethe_empty = gftools.bethe_gf_omega(iw_array-large_shift, half_bandwidth=D)
+    assert gftools.density(bethe_empty, potential=-large_shift, beta=beta) \
+        == pytest.approx(0.0, abs=1e-6)
+
+    #
+    # single site
+    #
+    assert gftools.density(iw_array, potential=0, beta=beta) == pytest.approx(0.5)
