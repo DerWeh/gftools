8634b87c51932ff58a5b94d68659854ecd85483c
Weh Andreas <andreas.weh@student.uni-augsburg.de>
2018-09-12 10:06:09 +0200

    Add `Decomposition` convenience class to matrix

--- gftools/matrix.py
+++ gftools/matrix.py
@@ -18,6 +18,102 @@ import numpy as np
 import scipy.linalg as la
 
 
+class Decomposition(object):
+    """Abstraction for matrix decomposition designed for green's functions.
+
+    This can be used as drop-in replacement for returning `rv, xi, rv_inv`.
+    Additionally it offers methods to reconstruct that matrix.
+    The intended use case is to use the `Decomposition` for the inversion of
+    the Green's function to calculate it from the resolvent.
+
+    Attributes
+    ----------
+    rv : (N, N) complex np.ndarray
+        The matrix of right eigenvalues.
+    xi : (N, ...) complex np.ndarray
+        The vector of eigenvalues
+    rv_inv : (N, N) complex np.ndarray
+        The inverse of `rv`.
+
+    TODO: include example
+    """
+    __slots__ = ('rv', 'xi', 'rv_inv')
+
+    def __init__(self, rv, xi, rv_inv):
+        self.rv = rv
+        self.xi = xi
+        self.rv_inv = rv_inv
+
+    def reconstruct(self, xi=None, kind='full'):
+        """Get matrix back from `Decomposition`.
+
+        If the reciprocal of `self.xi` was taken, this corresponds to the
+        inverse of the original matrix.
+        New axises can be appended to `self.xi`, however the 0th axis has to
+        correspond to the matrix dimension.
+
+        Parameters
+        ----------
+        xi : (N, ...) ndarray, optional
+            Alternative value used for `self.xi`. This argument can be used
+            instead of calling `self.apply` or modifying `self.xi`.
+        kind : {'diag', 'full'} or str
+            Defines how to reconstruct the matrix. If `kind` is 'diag',
+            only the diagonal elements are returned, if it is 'full' the
+            complete matrix is returned. The start of the strings is also
+            sufficient (e.g. `kind` = 'f').
+            Alternatively as `str` used for `np.einsum` can be given.
+
+        Returns
+        -------
+        reconstruct : (N, N, ...) or (N, ...) ndarray
+            The reconstructed matrix.
+
+        """
+        xi = xi if xi is not None else self.xi
+        kind = kind.lower()
+        if 'diag'.startswith(kind):
+            return self._reconstruct_diag(xi)
+        if 'full'.startswith(kind):
+            return self._reconstruct_full(xi)
+        return self._reconstruct_einsum(xi, sum_str=kind)
+
+    def _reconstruct_diag(self, xi):
+        if xi.ndim == 1:  # simple matrix case
+            return np.diagonal(np.matmul(self.rv*xi, self.rv_inv))
+        elif xi.shape[0] == self.rv.shape[0]:
+            return np.einsum('ij, j..., ji -> i...', self.rv, xi, self.rv_inv)
+        else:
+            raise ValueError("Shape of xi does not match:", xi.shape)
+
+    def _reconstruct_full(self, xi):
+        if xi.ndim == 1:  # simple matrix case
+            return np.matmul(self.rv*xi, self.rv_inv)
+        elif xi.shape[0] == self.rv.shape[0]:
+            return np.einsum('ij, j..., jk -> ik...', self.rv, xi, self.rv_inv)
+        else:
+            raise ValueError("Shape of xi does not match:", xi.shape)
+
+    def _reconstruct_einsum(self, xi, sum_str):
+        return np.einsum(sum_str, self.rv, xi, self.rv_inv)
+
+    def apply(self, func, *args, **kwds):
+        """Modify `self.xi` according to `func`.
+
+        Simple wrapper to transform the eigenvalues `self.xi`.
+        """
+        self.xi = func(self.xi, *args, **kwds)
+
+    def __iter__(self):
+        """Allow unpacking of the attributes."""
+        for attr in self.__slots__:
+            yield getattr(self, attr)
+
+    def __getitem__(self, key):
+        """Make `Decomposition` behave like the tuple `rv, xi, rv_inv`."""
+        return (self.rv, self.xi, self.rv_inv)[key]
+
+
 def decompose_gf_omega_symmetric(g_inv_band):
     r"""Decompose the Green's function into eigenvalues and eigenvectors.
     
