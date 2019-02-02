1040e25e34baa11f643397e37d6cdcf142086509
Weh Andreas <andreas.weh@student.uni-augsburg.de>
2019-02-02 21:11:22 +0100

    Add websites in setup

--- setup.py
+++ setup.py
@@ -8,25 +8,32 @@ def readme():
         return file_.read()
 
 
-setup(name="gftools",
-      version=versioneer.get_version(),
-      description="Collection of commonly used Green's functions and utilities",
-      long_description=readme(),
-      keywords="Green's\ function physics",
-      classifiers=[
-          'Intended Audience :: Science/Research',
-          'Programming Language :: Python :: 3.0',
-          'Programming Language :: Python :: 3.6',
-          'Topic :: Scientific/Engineering :: Physics',
-      ],
-      url="https://github.com/DerWeh/gftools",
-      author="Weh",
-      author_email="andreas.weh@student.uni-augsburg.de",
-      cmdclass=versioneer.get_cmdclass(),
-      packages=['gftools'],
-      install_requires=[
-          'numpy',
-          'scipy',
-      ],
-      setup_requires=['pytest-runner'],
-      tests_require=['pytest', 'hypothesis'],)
+setup(
+    name="gftools",
+    version=versioneer.get_version(),
+    description="Collection of commonly used Green's functions and utilities",
+    long_description=readme(),
+    keywords=r"Green's\ function physics",
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Physics',
+    ],
+    url="https://github.com/DerWeh/gftools",
+    project_urls={
+        "Documentation": "https://derweh.github.io/gftools/",
+        "ReadTheDocs": "https://gftools.readthedocs.io/en/latest/",
+        "Source Code": "https://github.com/DerWeh/gftools",
+    },
+    author="Weh",
+    author_email="andreas.weh@student.uni-augsburg.de",
+    cmdclass=versioneer.get_cmdclass(),
+    packages=['gftools'],
+    install_requires=[
+        'numpy',
+        'scipy',
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'hypothesis'],
+)
