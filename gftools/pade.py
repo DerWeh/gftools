Fix error calculation in pade for stacked calculation

gftools/pade.py
@@ -271,10 +271,10 @@ def Averager(z_in, coeff, n_min, n_max, valid_pades, kind='Gf'):
             pades = np.array(list(pade_iter))
             if _contains_nan(pades):
                 raise RuntimeError("Calculation of Pades failed, results contains NaNs")
-            pades[~valid_pades] = np.nan
+            pades[~valid_pades] = np.nan + 1j*np.nan
 
         pade_avg = np.nanmean(pades, axis=0)
-        std = np.nanstd(pades.real, axis=0, ddof=1) + 1j*np.nanstd(pades.real, axis=0, ddof=1)
+        std = np.nanstd(pades.real, axis=0, ddof=1) + 1j*np.nanstd(pades.imag, axis=0, ddof=1)
 
         if scalar_input:
             return Result(x=np.squeeze(pade_avg, axis=-1), err=np.squeeze(std, axis=-1))

gftools/tests/test_pade.py
@@ -1,14 +1,38 @@
-# TODO
-# from pathlib import Path
-# from sys import path as syspath
-# syspath.insert(1, str(Path('.').absolute().parent))
-# import gftools as gt
-# dir_ = Path('~/workspace/articles/master_thesis/data/m_hfm_m/multilayer/t02_mu045_h-9_UX/U08/1hf_19m_U08_poisson/output/00-G_omega.dat').expanduser()
-# test_data = np.loadtxt(dir_, unpack=True)
-# beta = 1. / 0.01
-# iws = gt.matsubara_frequencies(test_data[0], beta=beta)
-# omega = np.linspace(-4, 4., num=1000) + 1e-6j
-# gf_mid_iw = test_data[1] + 1j*test_data[2]
-# coeff = coefficients(iws, gf_mid_iw)
-# pade = np.array([calc(omega, iws, coeff, n_max=n) for n in range(590, 600, 2)])
-# pade2 = np.array([pade_ for pade_ in calc_iterator(omega, iws, coeff, n_min=589, n_max=599)])
+"""Test the functionality of the pade module."""
+import pytest
+import numpy as np
+
+from . import old_pade
+
+from .context import gftools as gt
+from .context import gt_pade
+
+
+def test_stacked_pade():
+    """Test results of calculating stacked Pades in parallel against single Pade."""
+    # Test setup
+    T = 0.037
+    D = 1.2
+    shift = 0.48
+    n_max = 800
+    iws = gt.matsubara_frequencies(np.arange(n_max), beta=1./T)
+    # disturb results to have invalid values in averaging
+    pseudo_nois = np.sin(iws.imag) * 2e-4
+    gf_bethe_iw = gt.bethe_gf_omega(iws, half_bandwidth=D) + pseudo_nois
+    gf_bethe_iw_shift = gt.bethe_gf_omega(iws+shift, half_bandwidth=D) + pseudo_nois
+    gf_bethe_fcts = np.array([gf_bethe_iw, gf_bethe_iw_shift])
+
+    # compare sequential and parallel calculation
+    coeff_single = np.array([gt_pade.coefficients(iws, fct_z=gf_bethe_iw),
+                             gt_pade.coefficients(iws, fct_z=gf_bethe_iw_shift)])
+    coeff_parall = gt_pade.coefficients(iws, fct_z=gf_bethe_fcts[np.newaxis])
+    assert np.all(coeff_single == coeff_parall[0])
+    avg_param = {'kind': 'Gf', 'n_min': 100, 'n_max': n_max, 'threshold': 1e-8}
+    omega = np.linspace(-D*1.5, D*1.5, num=1000) + iws[0]/10
+    avg_gf_z = [gt_pade.averaged(omega, z_in=iws, coeff=coeff_i, **avg_param)
+                for coeff_i in coeff_single]
+    avg_gf_z = gt.Result(x=np.array([avg_i.x for avg_i in avg_gf_z]),
+                         err=np.array([avg_i.err for avg_i in avg_gf_z]))
+    avg_gf_z_parall = gt_pade.averaged(omega, z_in=iws, coeff=coeff_parall, **avg_param)
+    assert np.all(avg_gf_z_parall.x[0] == avg_gf_z.x)
+    assert np.all(avg_gf_z_parall.err[0] == avg_gf_z.err)