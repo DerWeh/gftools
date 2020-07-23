fc71969d6ae15eb51ae416309c32585edaeff39c
Weh Andreas <andreas.weh@student.uni-augsburg.de>
2020-07-23 12:21:08 +0200

    BEB: add simple self-consistency test

--- gftool/tests/beb_test.py
+++ gftool/tests/beb_test.py
@@ -3,8 +3,10 @@ from functools import partial
 
 import pytest
 import numpy as np
+import hypothesis.strategies as st
 
 from scipy import integrate
+from hypothesis import given, assume
 
 from .context import gftool as gt
 
@@ -41,3 +43,25 @@ def test_gf_loc(self_beb_z):
     gf_cmp, __ = integrate.quad_vec(dos_gf_eps, -D, D)
 
     assert np.allclose(gf_z, gf_cmp)
+
+
+@given(z=st.complex_numbers(max_magnitude=1e-6))
+def test_selfconsistency(z):
+    """Self-consistent Gf is diagonal and equals averaged Gf."""
+    # randomly chosen example
+    assume(z.imag != 0)
+    z = np.array(z.conjugate() if z.imag < 0 else z)
+    t = np.array([[1.0, 0.3],
+                  [0.3, 1.2]])
+    eps = np.array([-0.137, 0.23])
+    c = np.array([0.137, 0.863])
+    D = 1.3
+    hilbert = partial(gt.bethe_hilbert_transform, half_bandwidth=D)
+    self_beb_z = gt.beb.solve_root(np.array(z), eps, concentration=c, hopping=t,
+                                   hilbert_trafo=hilbert, restricted=False)
+    gf_loc_z = gt.beb.gf_loc_z(z, self_beb_z, hopping=t, hilbert_trafo=hilbert, diagonal=False)
+    assert np.allclose(gf_loc_z[..., 0, 1], 0)
+    assert np.allclose(gf_loc_z[..., 1, 0], 0)
+    # Gf_loc diagonal -> inverse is 1/diagonal
+    gf_avg = c / (1./gt.beb.diagonal(gf_loc_z) + gt.beb.diagonal(self_beb_z) - eps)
+    assert np.allclose(gt.beb.diagonal(gf_loc_z), gf_avg)
