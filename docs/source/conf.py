46b6f39937a81e89cbe4fd7a7d4bb3597d04180b
Weh Andreas <andreas.weh@student.uni-augsburg.de>
2020-10-20 15:49:06 +0200

    Documentation: add button to toggle prompt (>>>)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -83,6 +83,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
+    'sphinx_toggleprompt',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
