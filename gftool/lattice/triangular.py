46213d4b48289156cf07b574e208416d19caed0b
Weh Andreas <andreas.weh@student.uni-augsburg.de>
2021-03-05 21:46:10 +0100

    Triangular: add half_bandwidth scaling

--- gftool/lattice/triangular.py
+++ gftool/lattice/triangular.py
@@ -4,8 +4,10 @@ The dispersion of the 2D triangular lattice is given by
 
 .. math:: ϵ_{k_x, k_y} = t [\cos(2k_x) + 2 \cos(k_x)\cos(k_y)]
 
+which can tack values :math:`ϵ_{k_x, k_y} ∈ [-1.5t, 3t]`.
+
 :half_bandwidth: The half-bandwidth `D` corresponds to a nearest neighbor hopping
-                 of FIXME:`t=D/3`.
+                 of `t=D/2.25`.
 
 """
 import numpy as np
@@ -20,13 +22,13 @@ def _signed_sqrt(z):
 
 
 def gf_z(z, half_bandwidth):
-    del half_bandwidth
-    z = np.asarray(z)
+    D = half_bandwidth * 4 / 9
+    z = 1.0 / D * np.asarray(z)
     shape = z.shape
     z = z.reshape(-1)
     advanced = z.imag < 0
     z = np.where(advanced, np.conj(z), z)  # calculate retarded only, and use symmetry
-    singular = z == -1  # try singularity which needs to be avoided
+    singular = D * z == -1  # try singularity which needs to be avoided
     z[singular] = 0  # mock value to avoid errors
     rr = _signed_sqrt(2*z + 3)
     gg = 4.0 / (_signed_sqrt(rr - 1)**3 * _signed_sqrt(rr + 3))  # eq (2.9)
@@ -35,6 +37,6 @@ def gf_z(z, half_bandwidth):
     K = np.asarray(_u_ellipk(mm))
     # eqs (2.22) and eq (2.18), fix correct plane
     K[kk.imag > 0] += 2j*_u_ellipk(1 - mm[kk.imag > 0])
-    gf_z = 1 / np.pi * gg * K  # eq (2.6)
+    gf_z = 1 / np.pi / D * gg * K  # eq (2.6)
     gf_z[singular] = 0 - 1j*np.infty
     return np.where(advanced, np.conj(gf_z), gf_z).reshape(shape)  # return to advanced by symmetry
