1ba162bcc33953e5a38a721d924d359063d2bb88
Weh Andreas <andreas.weh@student.uni-augsburg.de>
2021-03-05 21:30:01 +0100

    Triangular: fix implementation of Green's function

--- gftool/lattice/triangular.py
+++ gftool/lattice/triangular.py
@@ -13,11 +13,28 @@ import numpy as np
 from gftool._util import _u_ellipk
 
 
+def _signed_sqrt(z):
+    """Square root with correct sign for triangular lattice."""
+    sign = np.where((z.real < 0) & (z.imag < 0), -1, 1)
+    return sign * np.lib.scimath.sqrt(z)
+
+
 def gf_z(z, half_bandwidth):
     del half_bandwidth
-    sqrt = (2.0*z + 3)**0.5
-    g = 4.0 / ((sqrt - 1)**1.5 * (sqrt + 3.0)**0.5)
-    ksqr = sqrt * g**2
-    elliptic = _u_ellipk(ksqr)
-    gf_z = 1 / np.pi * g * elliptic
-    return gf_z
+    z = np.asarray(z)
+    shape = z.shape
+    z = z.reshape(-1)
+    advanced = z.imag < 0
+    z = np.where(advanced, np.conj(z), z)  # calculate retarded only, and use symmetry
+    singular = z == -1  # try singularity which needs to be avoided
+    z[singular] = 0  # mock value to avoid errors
+    rr = _signed_sqrt(2*z + 3)
+    gg = 4.0 / (_signed_sqrt(rr - 1)**3 * _signed_sqrt(rr + 3))  # eq (2.9)
+    kk = _signed_sqrt(rr) * gg  # eq (2.11)
+    mm = kk**2
+    K = np.asarray(_u_ellipk(mm))
+    # eqs (2.22) and eq (2.18), fix correct plane
+    K[kk.imag > 0] += 2j*_u_ellipk(1 - mm[kk.imag > 0])
+    gf_z = 1 / np.pi * gg * K  # eq (2.6)
+    gf_z[singular] = 0 - 1j*np.infty
+    return np.where(advanced, np.conj(gf_z), gf_z).reshape(shape)  # return to advanced by symmetry
