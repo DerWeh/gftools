07bd915c020398f33156416afb95b4100674e224
Weh Andreas <andreas.weh@student.uni-augsburg.de>
2021-03-07 21:38:36 +0100

    Honeycomb: add DOS

--- gftool/lattice/honeycomb.py
+++ gftool/lattice/honeycomb.py
@@ -59,3 +59,57 @@ def gf_z(z, half_bandwidth):
     D = half_bandwidth / 1.5
     z_rel = z / D
     return 2 / D * z_rel * triangular.gf_z(2*z_rel**2 - 1.5, half_bandwidth=9/4)
+
+
+def dos(eps, half_bandwidth):
+    r"""DOS of non-interacting 2D honeycomb lattice.
+
+    The DOS diverges at `eps=±half_bandwidth/3`.
+    The Green's function and therefore the DOS of the 2D honeycomb lattice can
+    be expressed in terms of the 2D triangular lattice
+    `gftool.lattice.triangular.dos`, see [horiguchi1972]_.
+
+    Parameters
+    ----------
+    eps : float np.ndarray or float
+        DOS is evaluated at points `eps`.
+    half_bandwidth : float
+        Half-bandwidth of the DOS, DOS(| `eps` | > `half_bandwidth`) = 0.
+        The `half_bandwidth` corresponds to the nearest neighbor hopping
+        :math:`t=4D/9`.
+
+    Returns
+    -------
+    dos : float np.ndarray or float
+        The value of the DOS.
+
+    See Also
+    --------
+    gftool.lattice.triangular.dos
+
+    References
+    ----------
+    .. [horiguchi1972] Horiguchi, T., 1972. Lattice Green’s Functions for the
+       Triangular and Honeycomb Lattices. Journal of Mathematical Physics 13,
+       1411–1419. https://doi.org/10.1063/1.1666155
+
+    Examples
+    --------
+    >>> eps = np.linspace(-1.5, 1.5, num=500)
+    >>> dos = gt.lattice.honeycomb.dos(eps, half_bandwidth=1)
+
+    >>> import matplotlib.pyplot as plt
+    >>> _ = plt.plot(eps, dos)
+    >>> _ = plt.axvline(-2/3, color='black', linewidth=0.8)
+    >>> _ = plt.axvline(+4/3, color='black', linewidth=0.8)
+    >>> _ = plt.xlabel(r"$\epsilon/D$")
+    >>> _ = plt.ylabel(r"DOS * $D$")
+    >>> _ = plt.axvline(0, color='black', linewidth=0.8)
+    >>> _ = plt.ylim(bottom=0)
+    >>> _ = plt.xlim(left=eps.min(), right=eps.max())
+    >>> plt.show()
+
+    """
+    D = half_bandwidth / 1.5
+    eps_rel = eps / D
+    return 2 / D * abs(eps_rel) * triangular.dos(2*eps_rel**2 - 1.5, half_bandwidth=9/4)
