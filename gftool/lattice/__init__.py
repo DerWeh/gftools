abccb1ff6554cc03eb1902e2c3a1227d86626abb
Weh Andreas <andreas.weh@student.uni-augsburg.de>
2021-05-31 22:46:24 +0200

    Doc: add description of quantities in lattice module

--- gftool/lattice/__init__.py
+++ gftool/lattice/__init__.py
@@ -1,4 +1,31 @@
-"""Collection of different lattices and their Green's functions.
+r"""Collection of different lattices and their Green's functions.
+
+The lattices are described by a tight binding Hamiltonian
+
+.. math:: H = t ∑_{⟨i,j⟩ σ} c^†_{iσ} c_{jσ},
+
+where :math:`t` is the hopping amplitude or integral.
+Mind the sign, often tight binding Hamiltonians are instead defined with a
+negative sign in front of :math:`t`.
+
+The Hamiltonian can be diagonalized
+
+.. math:: H = ∑_{kσ} ϵ_{k} c^†_{kσ} c_{kσ}.
+
+Typical quantities provided for the different lattices are:
+
+:`gf_z`: The one-particle Green's function
+
+   .. math:: G_{ii}(z) = ⟨⟨c_{iσ}|c^†_{iσ}⟩⟩(z) = 1/N ∑_k \frac{1}{z - ϵ_k}.
+
+:`dos`: The density of states (DOS)
+
+   .. math:: DOS(ϵ) = 1/N ∑_k δ(ϵ - ϵₖ).
+
+:`dos_moment`: The moments of the DOS
+
+    .. math:: ϵ^{(m)} = ∫dϵ DOS(ϵ) ϵ^m
+
 
 Submodules
 ----------
