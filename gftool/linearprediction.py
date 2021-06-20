e44227c40ebf65199f560c401516f7853fa7409e
Weh Andreas <andreas.weh@student.uni-augsburg.de>
2021-06-20 20:54:03 +0200

    LP: vectorize functions (untested)

--- gftool/linearprediction.py
+++ gftool/linearprediction.py
@@ -9,7 +9,12 @@ References
    https://doi.org/10.2200/S00086ED1V01Y200712SPR003
 
 """
+from functools import partial
+
 import numpy as np
+
+
+_gusum = partial(np.sum, axis=-1)
 
 
 def pcoeff_burg(x, order: int):
@@ -21,16 +26,16 @@ def pcoeff_burg(x, order: int):
 
     Parameters
     ----------
-    x : (N) complex np.ndarray
+    x : (..., N) complex np.ndarray
         Data of the (time) series to be predicted.
     order : int
         Prediction order, has to be smaller then `N`.
 
     Returns
     -------
-    a : (order) complex np.ndarray
+    a : (..., order) complex np.ndarray
         Prediction coefficients
-    rho : float
+    rho : (...) float np.ndarray
         Variance estimate.
 
     References
@@ -40,28 +45,30 @@ def pcoeff_burg(x, order: int):
 
     """
     N = x.shape[-1]
-    rho = np.full(order+1, np.nan)
-    rho[0] = np.sum(abs(x)**2) / N
-    eforw = x[1:]
-    eback = x[:-1]
-    a = np.full([order, order], np.nan, dtype=complex)
+    rho = np.full([*x.shape[:-1], order+1], np.nan)
+    rho[..., 0] = _gusum(abs(x)**2) / N
+    eforw = np.ascontiguousarray(x[..., 1:])
+    eback = np.ascontiguousarray(x[..., :-1])
+    a = np.full([*x.shape[:-1], order, order], np.nan, dtype=x.dtype)
     for k in range(1, order+1):
         # eq (7.38)
-        numer = -2*np.sum(eforw*np.conj(eback))
+        numer = -2*_gusum(eforw*np.conj(eback))
         # eq (7.42) for more efficient denom
         # if k > 1:
         #     denom = (1.0 -  np.abs(aa)**2)*denom - np.abs(eforw)
         # else:
-        denom = np.sum(abs(eforw)**2) + np.sum(abs(eback)**2)
+        denom = _gusum(abs(eforw)**2) + _gusum(abs(eback)**2)
         # eq (7.40)
-        kk = a[k-1, k-1] = numer / denom
-        rho[k] = (1.0 - abs(kk)**2) * rho[k-1]
+        kk = a[..., k-1, k-1] = numer / denom
+        rho[..., k] = (1.0 - abs(kk)**2) * rho[..., k-1]
+        kk = kk[..., np.newaxis]
         if k > 1:
-            a[k-1, :k-1] = a[k-2, :k-1] + kk*np.conj(a[k-2, k-2::-1])
+            a[..., k-1, :k-1] = a[..., k-2, :k-1] + kk*np.conj(a[..., k-2, k-2::-1])
         # eq (7.41)
-        eforw, eback = eforw[1:] + kk*eback[1:], eback[:-1] + np.conj(kk)*eforw[:-1]
-    sig2 = rho[-1]
-    a = a[-1]
+        eforw, eback = (eforw[..., 1:] + kk*eback[..., 1:],
+                        eback[..., :-1] + np.conj(kk)*eforw[..., :-1])
+    sig2 = rho[..., -1]
+    a = np.ascontiguousarray(a[..., -1, :])
     return a, sig2
 
 
@@ -70,23 +77,23 @@ def predict(x, pcoeff, num: int):
 
     Parameters
     ----------
-    x : (N) complex np.ndarray
+    x : (..., N) complex np.ndarray
         Data of the (time) series to be predicted.
-    pcoeff : (order) complex np.ndarray
+    pcoeff : (..., order) complex np.ndarray
         Prediction coefficients
     num : int
         Number of additional (time) steps.
 
     Returns
     -------
-    px : (N+num) complex np.ndarray
+    px : (..., N+num) complex np.ndarray
         Data of the (time) series extended by `num` steps, with
         `px[:x.size] = x`.
 
     """
-    start = x.size
-    order = pcoeff.size
-    xtended = np.concatenate([x, np.full(num, np.nan)], axis=-1)
+    start = x.shape[-1]
+    order = pcoeff.shape[-1]
+    xtended = np.concatenate([x, np.full([*x.shape[:-1], num], np.nan, dtype=x.dtype)], axis=-1)
     for ii in range(start, start+num):
-        xtended[ii] = -np.sum(pcoeff[::-1] * xtended[ii-order:ii])
+        xtended[..., ii] = -_gusum(pcoeff[..., ::-1] * xtended[..., ii-order:ii])
     return xtended
