5a7f13c16827385d1c2e22ae83fcadafccba16fd
Weh Andreas <andreas.weh@student.uni-augsburg.de>
2021-09-15 22:09:42 +0200

    Doc: use A4 paper for PDF

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,6 +27,7 @@ html_last_updated_fmt = '%Y-%d-%m'
 master_doc = 'index'
 
 latex_engine = 'xelatex'  # xelatex can handle Unicode
+papersize = 'a4paper'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
