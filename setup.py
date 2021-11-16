7ec4c2f28bcd05be58a8688402a37d4765dd3d9d
DerWeh <andreas.weh@web.de>
2021-11-16 14:46:23 +0100

    Officially support Python 3.10 (#22)
    * TEST: test against Python 3.10
    
    * Bump Sphinx version for Python 3.10 compatibility
    
    * Bump Python versions and add 3.10 tag

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Scientific/Engineering :: Physics',
     ],
     url="https://github.com/DerWeh/gftools",
