44bfbd670840cad1393008c618f2ed333db0718e
Weh Andreas <andreas.weh@student.uni-augsburg.de>
2022-04-26 14:26:04 +0200

    PolePade: fix tests

--- gftool/tests/polepade_test.py
+++ gftool/tests/polepade_test.py
@@ -1,10 +1,11 @@
 """Test PolePadé algorithm."""
 from functools import partial
+
 import numpy as np
 
 from .context import gftool as gt
 
-polepade = gt.polepade
+assert_allclose = np.testing.assert_allclose
 
 
 def test_bethe_unitcircle():
@@ -13,21 +14,21 @@ def test_bethe_unitcircle():
     # chose optimal sample points: the unit-(half-)circle
     z = np.exp(1j*np.linspace(np.pi, 0, num=252)[1:-1])
     gf_z = g0(z)
-    pade = polepade.continuation(z, gf_z)
+    pade = gt.polepade.continuation(z, gf_z)
 
     # check on the real axis
     # at the band-edge, it will obviously be hard, so we exclude it
     ww = np.linspace(-0.9, 0.9, num=500)
     gf_ww = g0(ww+1e-16j)
     pade_ww = pade.eval_polefct(ww)
-    np.allclose(gf_ww, pade_ww, rtol=1e-4)
+    assert_allclose(gf_ww, pade_ww, rtol=1e-4)
     pade_ww = pade.eval_zeropole(ww)
-    np.allclose(gf_ww, pade_ww, rtol=1e-4)
+    assert_allclose(gf_ww, pade_ww, rtol=1e-4)
 
     # check on the imaginary axis
     iws = gt.matsubara_frequencies(range(1024), beta=100)
     gf_iw = g0(iws)
     pade_iw = pade.eval_polefct(iws)
-    np.allclose(gf_iw, pade_iw)
+    assert_allclose(gf_iw, pade_iw, rtol=1e-6)
     pade_iw = pade.eval_zeropole(iws)
-    np.allclose(gf_iw, pade_iw)
+    assert_allclose(gf_iw, pade_iw, rtol=1e-6)
