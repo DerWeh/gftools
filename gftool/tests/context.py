5f45fccb1f43b7c06821ffa20e5d3e153b8de1a2
Weh Andreas <andreas.weh@student.uni-augsburg.de>
2022-12-04 10:06:09 +0100

    Test: allow higher dimensional input of allclose

--- gftool/tests/context.py
+++ gftool/tests/context.py
@@ -28,5 +28,7 @@ gftool.statistics._pade_frequencies = lru_cache(maxsize=10)(gftool.statistics._p
 
 def assert_allclose_vm(actual, desired, rtol=1e-7, atol=1e-14, **kwds):
     """Relax `assert_allclose` in case some elements are huge and others 0."""
-    fact = np.maximum(np.linalg.norm(desired, axis=-1), 1.0)
+    # TODO: we should provide an axis argument and somehow iterate...
+    # for now we just relax the test
+    fact = np.maximum(np.linalg.norm(desired), 1.0)
     np.testing.assert_allclose(actual, desired, rtol=rtol, atol=atol*fact, **kwds)
